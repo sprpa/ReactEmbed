1f1801cbd2e0bc29aa6508a724ff92999f97f316
Sai Ramya <sairamyakasu@gmail.com>
2024-02-19 16:55:35 +0530

    commit by sairamya

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 
 import './App.css';
+import About from './eis website/About';
 import Home from './eis website/Home';
 
 
@@ -8,7 +9,7 @@ import Home from './eis website/Home';
 function App() {
   return (
     <div className="App">
-   <Home/>
+   <About/>
      
     </div>
   );
