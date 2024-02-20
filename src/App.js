76f27f62442db8b6c023363094289778f0390d79
Akhila <your.email@example.com>
2024-02-20 10:49:55 +0530

    commit by akhila

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import About from './eis website/About';
 import Contact from './eis website/Contact';
 import Customer from './eis website/Customer';
 import ElectronicManufacturing from './eis website/ElectronicManufacturing';
+import Home from './eis website/Home';
 
 
 
 function App() {
   return (
     <div className="App">
-   <ElectronicManufacturing />
+   <Home />
      
     </div>
   );
